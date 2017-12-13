3a1831ad870df22481d0eaa9d5efd77a5ac151c4
Brandon <bjames9076@gmail.com>
2017-12-13 11:32:49 -0500

    refined timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,10 +5,11 @@
 var n = 30;
 setTimeout(countDown,1000);
 
+//quiz timmer
 function countDown(){
    n--;
    if(n > 0){
       setTimeout(countDown,1000);
    }
-   console.log(n);
+   document.getElementById("timer").innerHTML = n + " seconds left!";
 }
