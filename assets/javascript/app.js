a722c53717a83309e6f16ae817cdfa6caf689945
Brandon James <bjames9076@gmail.com>
2017-12-15 15:05:40 -0500

    didnt push before??

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,17 @@
 
 
 
-
+    //set time to 45 seconds
     var number = 5;
 
+    var countRight = 0;
+    var countWrong = 0;
+    var quest1;
+    var quest2;
+    var quest3;
+    var quest4;
+
+      //countdown and display time left
         function beginQuiz() {
           intervalId = setInterval(decrement, 1000);
         }
@@ -12,7 +20,57 @@
           number--;
           $("#timer").html("<h2>" + number + "</h2>");
           if (number === 0) {
-            stop();
-            alert("Time Up!");
+            clearInterval(intervalId);
+            gradeQuiz();
           }
         }
+
+        function question1(answer){
+          quest1 = answer;
+          if(quest1 === "a"){
+            countRight++;
+          }
+          else{
+            countWrong++;
+          }
+        }
+      
+        function question2(answer){
+          quest2 = answer;
+          if(quest1 === "b"){
+            countRight++;
+          }
+          else{
+            countWrong++;
+          }
+        }
+
+        function question3(answer){
+          quest3 = answer;
+          if(quest1 === "c"){
+            countRight++;
+          }
+          else{
+            countWrong++;
+          }
+        }
+
+        function question4(answer){
+          quest4 = answer;
+          if(quest1 === "a"){
+            countRight++;
+          }
+          else{
+            countWrong++;
+          }
+        }
+
+      //grade quiz once time reaches 0
+      function gradeQuiz(){
+        alert(countRight);
+        alert(countWrong);
+
+
+      }
+
+      
